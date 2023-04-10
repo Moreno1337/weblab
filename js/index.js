db9c55f424de519806e398bc98e81f1b4d3041e8
João Pedro Moreno <105954332+Moreno1337@users.noreply.github.com>
2023-04-10 14:25:01 -0300

    Header button timeout

--- js/index.js
+++ js/index.js
@@ -4,7 +4,15 @@ const menu = document.querySelector('.menu-lateral');
 botaoMenu.addEventListener('click', () => {
     if(menu.id.includes('menu-lateral-ativo')) {
         menu.removeAttribute('id', 'menu-lateral-ativo');
+        setTimeout( () => {
+            menu.classList.remove('menu-lateral-display-flex');
+            menu.classList.add('menu-lateral-display-none');
+        }, 300);
     } else {
-        menu.setAttribute('id', 'menu-lateral-ativo');
+        menu.classList.remove('menu-lateral-display-none');
+        menu.classList.add('menu-lateral-display-flex');
+        setTimeout( () => {
+            menu.setAttribute('id', 'menu-lateral-ativo');
+        }, 100);
     }
 });
