a7508cc6931daeda2c717919c767cc0dc8c6efd2
João Pedro Moreno <105954332+Moreno1337@users.noreply.github.com>
2023-04-10 13:17:54 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -2,5 +2,9 @@ const botaoMenu = document.querySelector('.cabecalho-menu');
 const menu = document.querySelector('.menu-lateral');
 
 botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu-lateral-ativo');
+    if(menu.id.includes('menu-lateral-ativo')) {
+        menu.removeAttribute('id', 'menu-lateral-ativo');
+    } else {
+        menu.setAttribute('id', 'menu-lateral-ativo');
+    }
 });
