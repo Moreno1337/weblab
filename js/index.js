9da3ed799a6196286fe0f1790021d787378fb7fd
ThiagoAndrade07 <120478086+ThiagoAndrade07@users.noreply.github.com>
2023-04-18 11:58:23 -0300

    Update Header

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-const botaoMenu = document.querySelector('.cabecalho-menu');
-const menu = document.querySelector('.menu-lateral');
+// const botaoMenu = document.querySelector('.cabecalho-menu');
+// const menu = document.querySelector('.menu-lateral');
 
-botaoMenu.addEventListener('click', () => {
-    if(menu.id.includes('menu-lateral-ativo')) {
-        menu.removeAttribute('id', 'menu-lateral-ativo');
-        setTimeout( () => {
-            menu.classList.remove('menu-lateral-display-flex');
-            menu.classList.add('menu-lateral-display-none');
-        }, 300);
-    } else {
-        menu.classList.remove('menu-lateral-display-none');
-        menu.classList.add('menu-lateral-display-flex');
-        setTimeout( () => {
-            menu.setAttribute('id', 'menu-lateral-ativo');
-        }, 100);
-    }
-});
+// botaoMenu.addEventListener('click', () => {
+//     if(menu.id.includes('menu-lateral-ativo')) {
+//         menu.removeAttribute('id', 'menu-lateral-ativo');
+//         setTimeout( () => {
+//             menu.classList.remove('menu-lateral-display-flex');
+//             menu.classList.add('menu-lateral-display-none');
+//         }, 300);
+//     } else {
+//         menu.classList.remove('menu-lateral-display-none');
+//         menu.classList.add('menu-lateral-display-flex');
+//         setTimeout( () => {
+//             menu.setAttribute('id', 'menu-lateral-ativo');
+//         }, 100);
+//     }
+// });
 
 const observer = new IntersectionObserver( entries => {
     entries.forEach( entry => {
